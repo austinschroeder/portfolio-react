0aa7ef394d7857489db66ac65eade612d81bde5b
austinschroeder <austinsoundy@gmail.com>
2021-06-06 14:01:52 -0700

    Add build directory

--- src/components/About.js
+++ src/components/About.js
@@ -13,7 +13,7 @@ function About() {
           </div>
           <div className="content-body">
             <p>I’m a software developer.  In a previous career, I worked with live concert touring and events.  Building a family has helped me realize the sacrifices involved with heavy traveling and touring.  Team building, coordinating logistics, and creating efficiency are things that are very important to me.  I seek a role as a software developer at a company where I can grow, and continue to master the craft.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p></p>
           </div>
         </div>
         <div className="side-content"></div>

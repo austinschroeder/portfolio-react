c568ecce5d6c625e0c18c68cd5656ac2a052dcf6
austinschroeder <austinsoundy@gmail.com>
2021-07-10 14:15:05 -0700

    Added link for resume PDF

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,5 @@
+import resume from '../static/resume.pdf'
+
 function NavBar() {
   const handleAboutClick = () => {
     document.getElementById('about-header').scrollIntoView({behavior:"smooth"});
@@ -10,6 +12,8 @@ function NavBar() {
       <a className="projects" href="#projects">Projects</a>
       <a className="experience" href="#experience">Experience</a>
       <a className="education" href="#education">Education</a>
+      <a className="resume" target="_blank" href={resume}>Resume</a>
+      {/* <a className="resume" target="_blank" href="https://docs.google.com/document/d/e/2PACX-1vQ9leWbR4c_KeiIzCIIascAYY24lgjNSMXmQPvriu8BM0vMhmzWqoupalsyG8lsZXS4IXOS7TlCDmUr/pub">Resume</a> */}
       {/* <a href="#contact">Contact</a> */}
     </div>
     // <!-- END NAVBAR -->

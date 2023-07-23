b6dd339832d01397425ef0816fee70d6319f9b53
austinschroeder <austinsoundy@gmail.com>
2023-07-23 12:17:48 -0700

    Update footer and header to styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,65 +1,157 @@
+import styled from 'styled-components';
+import backgroundGradient from '../images/gradient-bkgrnd.png';
+
+const ExternalLink = ({ href, children, ariaLabel }) => (
+  <StyledA href={href} target="_blank" rel="noreferrer" aria-label={ariaLabel}>
+    {children}
+  </StyledA>
+);
+
 const Header = () => {
   return (
-    // <!-- HEADER -->
-    <div className="jumbotron">
-      <div className="jumbotron-text">
+    <Jumbotron>
+      <JumbotronText>
         <h1>AUSTIN SCHROEDER</h1>
         <p>Full-Stack Web Developer in San Diego, CA</p>
-      </div>
-      <div className="jumbotron-social">
-        <ul className="ul-social">
-          <li className="li-social-links">
-            <a
-              className="social"
+      </JumbotronText>
+      <SocialContainer>
+        <ul>
+          <SocialLink>
+            <ExternalLink
               href="https://github.com/austinschroeder"
-              target="_blank"
-              rel="noreferrer"
+              ariaLabel="GitHub Profile"
             >
               <i className="fa fa-github" aria-hidden="true"></i>
-            </a>
-          </li>
-          <li className="li-social-links">
-            <a
-              className="social"
+            </ExternalLink>
+          </SocialLink>
+          <SocialLink>
+            <ExternalLink
               href="https://www.linkedin.com/in/austin-schroeder100/"
-              target="_blank"
-              rel="noreferrer"
+              ariaLabel="LinkedIn Profile"
             >
               <i className="fa fa-linkedin" aria-hidden="true"></i>
-            </a>
-          </li>
-          <li className="li-social-links">
-            <a
-              className="social"
+            </ExternalLink>
+          </SocialLink>
+          <SocialLink>
+            <ExternalLink
               href="https://twitter.com/austinshreddar"
-              target="_blank"
-              rel="noreferrer"
+              ariaLabel="Twitter Profile"
             >
               <i className="fa fa-twitter" aria-hidden="true"></i>
-            </a>
-          </li>
-          <li className="li-social-links">
-            <a
-              className="social"
-              href="mailto:austinsoundy@gmail.com"
-              target="_blank"
-              rel="noreferrer"
+            </ExternalLink>
+          </SocialLink>
+          <SocialLink>
+            <ExternalLink
+              href="mailto:
+              austinsoundy@gmail.com"
+              ariaLabel="Email Address"
             >
               <i className="fa fa-envelope-o" aria-hidden="true"></i>
-            </a>
-          </li>
+            </ExternalLink>
+          </SocialLink>
         </ul>
-      </div>
-      <div id="down-arrow">
-        <span>
-          <a href="#about">
-            <i className="fa fa-chevron-down" aria-hidden="true"></i>
-          </a>
-        </span>
-      </div>
-    </div>
-    //   <!-- END HEADER -->
+      </SocialContainer>
+      <ArrowLink>
+        <a href="#about" aria-label="About Section">
+          <i className="fa fa-chevron-down" aria-hidden="true"></i>
+        </a>
+      </ArrowLink>
+    </Jumbotron>
   );
 };
+
+const StyledA = styled.a`
+  font-size: 3em;
+  color: white;
+  padding: 20px;
+
+  &:hover {
+    text-shadow: 2px 2px 5px white;
+  }
+`;
+
+const Jumbotron = styled.div`
+  height: 100vh;
+  background: #054c61 url(${backgroundGradient});
+  background-repeat: no-repeat;
+  background-size: cover;
+  margin-bottom: 0;
+  display: flex;
+  flex-direction: column;
+
+  @media screen and (max-width: 600px) {
+    height: 600px;
+  }
+`;
+
+const JumbotronText = styled.div`
+  font-weight: 400;
+  font-size: 2em;
+  color: white;
+  text-align: center;
+  padding-top: 150px;
+
+  h1 {
+    font-weight: 800;
+    font-size: 3em;
+    margin-bottom: 0;
+    user-select: none;
+    @media screen and (max-width: 600px) {
+      font-size: 3rem;
+    }
+  }
+  p {
+    margin: 0;
+    user-select: none;
+    @media screen and (max-width: 600px) {
+      font-size: 1.5rem;
+    }
+  }
+
+  @media screen and (max-width: 600px) {
+    /* font-size: 1.5em; */
+    /* border: 1px solid yellow; */
+    padding-left: 20px;
+    padding-right: 20px;
+  }
+`;
+
+const SocialContainer = styled.nav`
+  text-align: center;
+  padding-left: 0;
+
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+`;
+
+const SocialLink = styled.li`
+  display: inline-block;
+
+  /* a {
+    font-size: 3em;
+    color: white;
+    padding: 20px;
+
+    &:hover {
+      text-shadow: 2px 2px 5px white;
+    }
+  } */
+`;
+
+const ArrowLink = styled.div`
+  margin-top: auto;
+  text-align: center;
+  margin-bottom: 50px;
+
+  a {
+    color: white;
+
+    &:hover {
+      text-shadow: 2px 2px 5px white;
+    }
+  }
+`;
 
 export default Header;
